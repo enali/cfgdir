23296a34069ee78740f35a3f917dac4b3975c789
enali <enalix@163.com>
2016-01-29 09:38:06 +0800

    first commit

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cfgdir=`pwd`
+
+ln -s $cfgdir/vimrc ~/.vimrc
+ln -s $cfgdir/gvimrc ~/.gvimrc
+ln -s $cfgdir/tmux.conf ~/.tmux.conf
+ln -s $cfgdir/bash_aliases ~/.bash_aliases
+ln -s $cfgdir/bash_env ~/.bash_env
